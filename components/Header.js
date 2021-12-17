ad43f2ea52b52b80c636cfc90c9f1a72e937a4e2
Thom Krupa <thom@bejamas.io>
2021-12-17 18:46:43 +0100

    style: make logo centered

--- components/Header.js
+++ components/Header.js
@@ -4,9 +4,9 @@ export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
       <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
-      <p className="text-2xl dark:text-white">
+      <p className="text-2xl dark:text-white text-center">
         <Link href="/">
-          <a id="blog-name">{name}</a>
+          <a>{name}</a>
         </Link>
       </p>
     </header>
