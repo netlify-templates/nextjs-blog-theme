e43d5b83920303d6e8edacc14a24dea01368e33d
Marco Valsecchi <marco.valsecchi@gmail.com>
2022-02-25 14:17:17 +0100

    Fix overflow-x on mobile (#26)
    Apply the overflow to both the axis to prevent additional desktop vertical scrollbars

--- components/Layout.js
+++ components/Layout.js
@@ -50,7 +50,7 @@ export default function Layout({ children }) {
   }, []);
 
   return (
-    <div className="relative pb-24">
+    <div className="relative pb-24 overflow-hidden">
       <div className="flex flex-col items-center max-w-2xl w-full mx-auto">
         {children}
       </div>
