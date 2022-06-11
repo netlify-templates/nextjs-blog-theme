9b548706e9e9790464154755b75b6b19b8d45b5e
Tara Z. Manicsic <tzmanics@gmail.com>
2022-06-11 00:49:24 -0400

    adds cypress, config, and test

---
+++ cypress/e2e/basic.cy.js
@@ -0,0 +1,9 @@
+describe('empty spec', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('displays the title text', () => {
+    cy.get('h1').contains('Next.js Blog Theme');
+  });
+});
