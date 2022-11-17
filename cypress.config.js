3edbe2ebb50bd9cac23f0d2946781122fe5bf380
Marc Littlemore <marc.littlemore@netlify.com>
2022-11-17 11:51:01 +0000

    fix: revert cypress change for now

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,8 @@
 const { defineConfig } = require('cypress');
 
-// Use Netlify deploy preview URL if we have it
-const baseUrl = process.env.DEPLOY_PRIME_URL || 'http://localhost:3000';
-
 module.exports = defineConfig({
   e2e: {
-    baseUrl,
+    baseUrl: 'http://localhost:3000',
     supportFile: false,
   },
 });
