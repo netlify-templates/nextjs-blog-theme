887ee44f23a9b36862dfc635eb4c3b9c9ec08175
Marc Littlemore <marc.littlemore@netlify.com>
2022-11-17 11:37:50 +0000

    fix: run cypress tests against deploy preview

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,11 @@
 const { defineConfig } = require('cypress');
 
+// Use Netlify deploy preview URL if we have it
+const baseUrl = process.env.DEPLOY_PRIME_URL || 'http://localhost:3000';
+
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl,
     supportFile: false,
   },
 });
