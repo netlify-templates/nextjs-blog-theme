a075d5ad43fdbb533f483bd2a6e14975af20d484
taty2010 <shydalys123@gmail.com>
2023-01-04 10:02:46 -0600

    updated cypress projectId

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  projectId: "dzuvmt",
+  projectId: "spdb6n",
   e2e: {
     baseUrl: 'http://localhost:3000',
     supportFile: false,
