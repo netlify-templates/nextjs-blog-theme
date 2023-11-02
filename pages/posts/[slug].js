7065eda9d9e86a407d1ca73ac640cbb8278057e6
Rémi Weng <ixartz@users.noreply.github.com>
2023-11-03 00:38:49 +0100

    style: in posts page place href attribute before the long classname from Tailwind CSS

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -58,7 +58,7 @@ export default function PostPage({
         </main>
         <div className="grid md:grid-cols-2 lg:-mx-24 mt-12">
           {prevPost && (
-            <Link className='py-8 px-10 text-center md:text-right first:rounded-t-lg md:first:rounded-tr-none md:first:rounded-l-lg last:rounded-r-lg first last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 last:border-t md:border-r-0 md:last:border-r md:last:rounded-r-none flex flex-col' href={`/posts/${prevPost.slug}`}>
+            <Link href={`/posts/${prevPost.slug}`} className='py-8 px-10 text-center md:text-right first:rounded-t-lg md:first:rounded-tr-none md:first:rounded-l-lg last:rounded-r-lg first last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 last:border-t md:border-r-0 md:last:border-r md:last:rounded-r-none flex flex-col' >
               <p className="uppercase text-gray-500 mb-4 dark:text-white dark:opacity-60">
                 Previous
               </p>
@@ -69,7 +69,7 @@ export default function PostPage({
             </Link>
           )}
           {nextPost && (
-            <Link className='py-8 px-10 text-center md:text-left md:first:rounded-t-lg last:rounded-b-lg first:rounded-l-lg md:last:rounded-bl-none md:last:rounded-r-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-t-0 first:border-t first:rounded-t-lg md:border-t border-b-0 last:border-b flex flex-col' href={`/posts/${nextPost.slug}`}>
+            <Link href={`/posts/${nextPost.slug}`} className='py-8 px-10 text-center md:text-left md:first:rounded-t-lg last:rounded-b-lg first:rounded-l-lg md:last:rounded-bl-none md:last:rounded-r-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-t-0 first:border-t first:rounded-t-lg md:border-t border-b-0 last:border-b flex flex-col' >
               <p className="uppercase text-gray-500 mb-4 dark:text-white dark:opacity-60">
                 Next
               </p>
