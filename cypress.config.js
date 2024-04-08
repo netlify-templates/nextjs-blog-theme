5200894e24afcd8d97041c09fa93815385a08a81
Elad Rosenheim <elad.rosenheim@gmail.com>
2024-04-08 15:18:02 +0300

    Bumps & cleanups
    * Upgrade Link to Next.js 14 style
    * Remove Cypress

--- cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('cypress');
-
-module.exports = defineConfig({
-  projectId: "spdb6n",
-  e2e: {
-    baseUrl: 'http://localhost:3000',
-    supportFile: false,
-  },
-});
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -58,30 +58,34 @@ export default function PostPage({
         </main>
         <div className="grid md:grid-cols-2 lg:-mx-24 mt-12">
           {prevPost && (
-            <Link href={`/posts/${prevPost.slug}`}>
-              <a className="py-8 px-10 text-center md:text-right first:rounded-t-lg md:first:rounded-tr-none md:first:rounded-l-lg last:rounded-r-lg first last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 last:border-t md:border-r-0 md:last:border-r md:last:rounded-r-none flex flex-col">
-                <p className="uppercase text-gray-500 mb-4 dark:text-white dark:opacity-60">
-                  Previous
-                </p>
-                <h4 className="text-2xl text-gray-700 mb-6 dark:text-white">
-                  {prevPost.title}
-                </h4>
-                <ArrowIcon className="transform rotate-180 mx-auto md:mr-0 mt-auto" />
-              </a>
-            </Link>
+            (<Link
+              href={`/posts/${prevPost.slug}`}
+              className="py-8 px-10 text-center md:text-right first:rounded-t-lg md:first:rounded-tr-none md:first:rounded-l-lg last:rounded-r-lg first last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 last:border-t md:border-r-0 md:last:border-r md:last:rounded-r-none flex flex-col">
+
+              <p className="uppercase text-gray-500 mb-4 dark:text-white dark:opacity-60">
+                Previous
+              </p>
+              <h4 className="text-2xl text-gray-700 mb-6 dark:text-white">
+                {prevPost.title}
+              </h4>
+              <ArrowIcon className="transform rotate-180 mx-auto md:mr-0 mt-auto" />
+
+            </Link>)
           )}
           {nextPost && (
-            <Link href={`/posts/${nextPost.slug}`}>
-              <a className="py-8 px-10 text-center md:text-left md:first:rounded-t-lg last:rounded-b-lg first:rounded-l-lg md:last:rounded-bl-none md:last:rounded-r-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-t-0 first:border-t first:rounded-t-lg md:border-t border-b-0 last:border-b flex flex-col">
-                <p className="uppercase text-gray-500 mb-4 dark:text-white dark:opacity-60">
-                  Next
-                </p>
-                <h4 className="text-2xl text-gray-700 mb-6 dark:text-white">
-                  {nextPost.title}
-                </h4>
-                <ArrowIcon className="mt-auto mx-auto md:ml-0" />
-              </a>
-            </Link>
+            (<Link
+              href={`/posts/${nextPost.slug}`}
+              className="py-8 px-10 text-center md:text-left md:first:rounded-t-lg last:rounded-b-lg first:rounded-l-lg md:last:rounded-bl-none md:last:rounded-r-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-t-0 first:border-t first:rounded-t-lg md:border-t border-b-0 last:border-b flex flex-col">
+
+              <p className="uppercase text-gray-500 mb-4 dark:text-white dark:opacity-60">
+                Next
+              </p>
+              <h4 className="text-2xl text-gray-700 mb-6 dark:text-white">
+                {nextPost.title}
+              </h4>
+              <ArrowIcon className="mt-auto mx-auto md:ml-0" />
+
+            </Link>)
           )}
         </div>
       </article>
