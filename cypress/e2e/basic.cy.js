621e468f9e166b1daf662cde33f4c776ee224e9f
Elad Rosenheim <elad.rosenheim@gmail.com>
2024-04-08 15:09:23 +0300

    Bump versions

--- cypress/e2e/basic.cy.js
+++
@@ -1,9 +0,0 @@
-describe('sample render test', () => {
-  beforeEach(() => {
-    cy.visit('/');
-  });
-
-  it('displays the title text', () => {
-    cy.get('h1').contains('Next.js Blog Theme');
-  });
-});
