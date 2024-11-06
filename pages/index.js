ba8233335da4ff878051dfd50c624126ab3c46b6
TomasBankauskas <tbdizainas@gmail.com>
2024-11-06 10:28:42 +0200

    added visual editor, updated content

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Index({ posts, globalData }) {
           {posts.map((post) => (
             <li
               key={post.filePath}
-              className="transition bg-white border border-b-0 border-gray-800 md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 dark:border-white border-opacity-10 dark:border-opacity-10 last:border-b hover:border-b hovered-sibling:border-t-0"
+              className="transition bg-white border border-b-0 border-gray-800 md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 dark:border-white border-opacity-10 dark:border-opacity-10 last:border-b hover:border-b hovered-sibling:border-t-0" data-sb-object-id={`posts/${post.filePath}`}
             >
               <Link
                 as={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`}
@@ -29,13 +29,13 @@ export default function Index({ posts, globalData }) {
                 className="block px-6 py-6 lg:py-10 lg:px-16 focus:outline-none focus:ring-4">
 
                 {post.data.date && (
-                  <p className="mb-3 font-bold uppercase opacity-60">
+                  <p className="mb-3 font-bold uppercase opacity-60" data-sb-field-path="date">
                     {post.data.date}
                   </p>
                 )}
-                <h2 className="text-2xl md:text-3xl">{post.data.title}</h2>
+                <h2 className="text-2xl md:text-3xl" data-sb-field-path="title">{post.data.title}</h2>
                 {post.data.description && (
-                  <p className="mt-3 text-lg opacity-60">
+                  <p className="mt-3 text-lg opacity-60" data-sb-field-path="description">
                     {post.data.description}
                   </p>
                 )}
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -34,7 +34,10 @@ export default function PostPage({
   prevPost,
   nextPost,
   globalData,
+  slug
 }) {
+
+
   return (
     <Layout>
       <SEO
@@ -42,17 +45,17 @@ export default function PostPage({
         description={frontMatter.description}
       />
       <Header name={globalData.name} />
-      <article className="px-6 md:px-0">
+      <article className="px-6 md:px-0" data-sb-object-id={`posts/${slug}.mdx`}>
         <header>
-          <h1 className="mb-12 text-3xl text-center md:text-5xl dark:text-white">
+          <h1 className="mb-12 text-3xl text-center md:text-5xl dark:text-white" data-sb-field-path="title">
             {frontMatter.title}
           </h1>
           {frontMatter.description && (
-            <p className="mb-4 text-xl">{frontMatter.description}</p>
+            <p className="mb-4 text-xl" data-sb-field-path="description">{frontMatter.description}</p>
           )}
         </header>
         <main>
-          <article className="prose dark:prose-dark">
+          <article className="prose dark:prose-dark" data-sb-field-path="markdown_content">
             <MDXRemote {...source} components={components} />
           </article>
         </main>
@@ -113,6 +116,7 @@ export const getStaticProps = async ({ params }) => {
       globalData,
       source: mdxSource,
       frontMatter: data,
+      slug: params.slug,
       prevPost,
       nextPost,
     },
