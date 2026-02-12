ea28f4be17a7afc360334758b56040f5faf28a43
Tomas Bankauskas <tomas.bankauskas@netlify.com>
2026-02-12 14:31:13 +0200

    Fix optional chaining for process.env in theme-utils.js (#365)

--- utils/theme-utils.js
+++ utils/theme-utils.js
@@ -1,8 +1,8 @@
 const { COLOR_THEMES, FONT_THEMES } = require('../themes');
 
-const THEME = process.env.BLOG_THEME || 'default';
-const FONT_HEADINGS = process.env.BLOG_FONT_HEADINGS || 'sans-serif';
-const FONT_BODY = process.env.BLOG_FONT_BODY || 'sans-serif';
+const THEME = process.env?.BLOG_THEME || 'default';
+const FONT_HEADINGS = process.env?.BLOG_FONT_HEADINGS || 'sans-serif';
+const FONT_BODY = process.env?.BLOG_FONT_BODY || 'sans-serif';
 
 export function generateCssVariables() {
   const cssVars = {};
